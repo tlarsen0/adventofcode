2f60aa3cd3265b18b52a9d9b1a38b884e424a627
Tom Larsen <tom_larsen@msn.com>
2021-12-07 22:44:54 -0600

    Adding configuration and solution for Advent of Code 2021 Day 7 Part 2.

---
+++ 2021/day7-2/src/main/kotlin/Main.kt
@@ -0,0 +1,54 @@
+import java.io.File
+
+fun main(args: Array<String>) {
+    println("AOC 2021, Day 7.2 starting!!!!")
+
+    val crabCast = CrabCast()
+    File(args[0]).forEachLine {
+        for(pos in it.split(",")) {
+            crabCast.addCrab(pos.toInt())
+        }
+    }
+
+    val leastFuel = crabCast.leastEnergyUsed()
+    println("Least amount of fuel spent: $leastFuel")
+
+    println("AOC 2021, Day 7.2 complete!!!!")
+}
+
+// Evidently a group of crabs is a cast.
+class CrabCast {
+    private val crabs = ArrayList<Int>()
+    private var minPosition = 0
+    private var maxPosition = 0
+
+    fun addCrab(position : Int) {
+        crabs.add(position)
+        minPosition = minPosition.coerceAtMost(position)
+        maxPosition = maxPosition.coerceAtLeast(position)
+    }
+
+    fun leastEnergyUsed() : Int {
+        var minEnergy = -1
+        for(i in minPosition .. maxPosition) {
+            var energy = 0
+            for (crab in crabs) {
+                energy += cumulativeEnergy(Math.abs(crab - i))
+            }
+            println("At position $i, energy used $energy")
+            if(minEnergy == -1)
+                minEnergy = energy
+            else
+                minEnergy = minEnergy.coerceAtMost(energy)
+        }
+        return minEnergy
+    }
+
+    private fun cumulativeEnergy(steps : Int) : Int {
+        var energy = 0
+        for(i in 0 .. steps) {
+            energy += i
+        }
+        return energy
+    }
+}
