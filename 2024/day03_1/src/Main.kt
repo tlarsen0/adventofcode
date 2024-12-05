c97ea16f032b5810d1f47fe2406f2d5c6cead10b
tlarsen0 <72167435+tlarsen0@users.noreply.github.com>
2024-12-05 07:33:15 -0600

    Final configuration, code, and solution to Advent of Code 2024 Day 3, Part 1.

---
+++ 2024/day03_1/src/Main.kt
@@ -0,0 +1,34 @@
+import java.io.File
+
+// important note: RegEx that handles the problem: mul\(\d{1,3},\d{1,3}\)
+fun main(args: Array<String>) {
+    println("AOC 2024, Day 3, Part 1 starting!!!!")
+
+    val theOperations = MulOperations()
+    val theRegex = "mul\\(\\d{1,3},\\d{1,3}\\)".toRegex()
+    File(args[0]).forEachLine {
+        val theMatches = theRegex.findAll(it)
+        for(match in theMatches) {
+            println("tlarsen,L11: match = ${match.value}")
+            theOperations.dataList.add(match.value)
+        }
+    }
+
+    println("The answer: ${theOperations.results()}")
+
+    println("AOC 2024, Day 3, Part 1 completed!!!")
+}
+
+class MulOperations {
+    val dataList = ArrayList<String>()
+
+    fun results():Long {
+        var theResult = 0L
+        val regex = "(\\d{1,3})".toRegex()
+        for(d in dataList) {
+            val theMatches = regex.findAll(d)
+            theResult += (theMatches.first().value.toLong() * theMatches.last().value.toLong())
+        }
+        return theResult
+    }
+}
