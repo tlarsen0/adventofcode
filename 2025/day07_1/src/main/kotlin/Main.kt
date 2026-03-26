e29f30c01c896ac626b53dcad7bb7812166624e2
tlarsen0 <72167435+tlarsen0@users.noreply.github.com>
2026-03-26 09:42:47 -0500

    Adding comments and a build tweak.

--- 2025/day07_1/src/main/kotlin/Main.kt
+++ 2025/day07_1/src/main/kotlin/Main.kt
@@ -2,6 +2,12 @@ package org.tlarsen.adventofcode2025.day07.part1
 
 import java.io.File
 
+/**
+ * Entry point for the Advent of Code 2025 Day 7 Part 1 challenge.
+ * This program reads a manifold configuration from a file and calculates beam splits.
+ *
+ * Usage: Main <input_file_path>
+ */
 fun main(args: Array<String>) {
     println("AOC 2025, Day 7, Part 1 starting!!!!")
     println("Details of problem are here: https://adventofcode.com/2025/day/7#part1")
